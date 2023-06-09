40772f10e947fb9ee693d76beaaabf66154ac87e
Pawan1208cse <125536864+Pawan1208cse@users.noreply.github.com>
2023-06-10 01:00:08 +0530

    Add files via upload

---
+++ tsk1.js
@@ -0,0 +1,5 @@
+var array = [3, 1, 4, 2, 5];
+array.sort(function(a, b) {
+  return a - b;
+});
+console.log(array);
---
+++ tsk2.js
@@ -0,0 +1,4 @@
+var array1 = [1, 2, 3];
+var array2 = [4, 5, 6];
+var newArray = array1.concat(array2);
+console.log(newArray);
